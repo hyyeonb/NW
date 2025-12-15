b05647f4f03be44b8bc5df84e3a0b164eaa48876
hyyeonb <hyeonbin@stninfotech.com>
2025-12-15 17:33:22 +0900

    모델 별 OID 등록 페이지 추가

--- src/api/index.js
+++ src/api/index.js
@@ -2,3 +2,4 @@ export { default as apiClient } from './client';
 export { authApi } from './auth';
 export { groupsApi } from './groups';
 export { devicesApi } from './devices';
+export { snmpApi } from './snmp';
